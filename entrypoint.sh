a98fb7c666b2fc006653427ae7dea6d83e0bf5c4
CoreOS Admin <core@earthcube-workbench.os.ncsa.edu>
2017-05-23 18:31:44 +0000

    Fixed postgres environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ cp -r /stromatolites_demo /workspace/
 cd /workspace/stromatolites_demo/
 
 sed -i s/POSTGRES_USER/$POSTGRES_USER/g credentials
-sed -i s/POSTGRES_PORT/$POSTGRES_SERVICE_PORT_5432_PORT/g credentials
-sed -i s/POSTGRES_HOST/$POSTGRES_SERVICE_PORT_5432_ADDR/g credentials
+sed -i s/POSTGRES_PORT/$POSTGRES_PORT_5432_TCP_PORT/g credentials
+sed -i s/POSTGRES_HOST/$POSTGRES_PORT_5432_TCP_ADDR/g credentials
 sed -i s/POSTGRES_DB/$POSTGRES_DB/g credentials
 sed -i s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/g credentials
 
